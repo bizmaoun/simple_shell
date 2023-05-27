e3d49f264d279d6ec3f0f891c7ed8bed148ec378
Si2lia <soukaina.mourisse02@gmail.com>
2023-05-26 17:04:39 -0700

    adding the mylist2 to the repo

---
+++ mylist2.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * myadd_rvar_node - Adds a variable at the end
+ * of a r_var list.
+ * @head: head of the linked list.
+ * @lvar: length of the variable.
+ * @val: value of the variable.
+ * @lval: length of the value.
+ * Return: the address of the head.
+ */
+r_var *myadd_rvar_node(r_var **head, int lvar, char *val, int lval)
+{
+	r_var *new, *temp;
+
+	new = malloc(sizeof(r_var));
+	if (new == NULL)
+		return (NULL);
+
+	new->len_var = lvar;
+	new->val = val;
+	new->len_val = lval;
+
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+
+	return (*head);
+}
+
+/**
+ * myfree_rvar_list - Frees a r_var list
+ * @head: head of the linked list.
+ * Return: void
+ */
+void myfree_rvar_list(r_var **head)
+{
+	r_var *temp;
+	r_var *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
