1ccc1b886380e5a486b81677ba240d0ed4f3e4ce
Si2lia <soukaina.mourisse02@gmail.com>
2023-05-26 17:14:37 -0700

    adding the mystdlib to the repo

---
+++ mystdlib.c
@@ -0,0 +1,100 @@
+#include "main.h"
+
+/**
+ * myget_len - The lenght of a number.
+ * @n: type int number.
+ * Return: the lenght of a number.
+ */
+int myget_len(int n)
+{
+	unsigned int n1;
+	int lenght = 1;
+
+	if (n < 0)
+	{
+		lenght++;
+		n1 = n * -1;
+	}
+	else
+	{
+		n1 = n;
+	}
+	while (n1 > 9)
+	{
+		lenght++;
+		n1 = n1 / 10;
+	}
+
+	return (lenght);
+}
+/**
+ * myaux_itoa - Converts int to string.
+ * @n: type int number
+ * Return: a string.
+ */
+char *myaux_itoa(int n)
+{
+	unsigned int n1;
+	int lenght = myget_len(n);
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * (lenght + 1));
+	if (buffer == 0)
+		return (NULL);
+
+	*(buffer + lenght) = '\0';
+
+	if (n < 0)
+	{
+		n1 = n * -1;
+		buffer[0] = '-';
+	}
+	else
+	{
+		n1 = n;
+	}
+
+	lenght--;
+	do {
+		*(buffer + lenght) = (n1 % 10) + '0';
+		n1 = n1 / 10;
+		lenght--;
+	}
+	while (n1 > 0)
+		;
+	return (buffer);
+}
+
+/**
+ * _myatoi - Converts a string to integer.
+ * @s: input string.
+ * Return: Integer.
+ */
+int _myatoi(char *s)
+{
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if (((s + count) >= '0') && ((s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
+}
